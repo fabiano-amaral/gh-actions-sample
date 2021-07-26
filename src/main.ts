e206df38ad81a7f580c252194846923063bc8726
Fabiano Amaral <fabiano.af@outlook.com>
2021-07-26 16:44:14 -0300

    chore: add var

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  var nome = fabiano;
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
